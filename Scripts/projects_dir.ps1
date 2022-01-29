ef8ac44de8794ec09244c3fdba0148527556f125
Lin Wang <wanglinseven@gmail.com>
2022-01-29 15:23:35 -0500

    improve pd powershell command

--- Scripts/projects_dir.ps1
+++ Scripts/projects_dir.ps1
@@ -1,12 +1,38 @@
 $ArgName = $Args[0]
 
-$ProjectNames = @{ `
+$ProjectNames_WorkPC = @{ `
     "dd" = "C:\AMD_Repos\devdriver"; `
     "ddtool" = "C:\AMD_Repos\internal-tools"; `
     "pal" = "C:\AMD_Repos\pal"; `
     "dot" = "C:\Projects\DotFiles"; `
     "d3dsamples" = "C:\Projects\D3D12_Samples\Samples\Desktop"; `
     "prj" =  "C:\Projects"
+}
+
+$ProjectNames_WorkLaptop = @{ `
+    "dd" = "C:\AMD_Repos\devdriver"; `
+    "ddtool" = "C:\AMD_Repos\internal-tools"; `
+    "pal" = "C:\AMD_Repos\pal"; `
+    "dot" = "C:\Projects\DotFiles"; `
+    "d3dsamples" = "C:\Projects\D3D12_Samples\Samples\Desktop"; `
+    "prj" =  "C:\Projects"
+}
+
+$ProjectNames_PersonalLaptop = @{ `
+    "prj" =  "C:\Projects"
+    "dot" = "C:\Projects\DotFiles"
+}
+
+$HostName = [System.Net.Dns]::GetHostName()
+
+if ($HostName -eq "LAPTOP-F9KFD4OS") {
+    $ProjectNames = $ProjectNames_PersonalLaptop
+} elseif ($HostName = "workpc") {
+    $ProjectNames = $ProjectNames_WorkPC
+} elseif ($HostName = "worklaptop") {
+    $ProjectNames = $ProjectNames_WorkLaptop
+} else {
+    Write-Output "Unrecognized host name: $HostName."
 }
 
 if ($ArgName -ne $null -and $ProjectNames.ContainsKey($ArgName)) {

dc5970958c9ed8e618986ab79ae69aece8d38a70
Lin Wang <wanglinseven@gmail.com>
2026-05-13 20:15:43 +0800

    update telescope.nvim

--- init.lua
+++ init.lua
@@ -111,7 +111,7 @@ require('lazy').setup({
   {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
+    tag = 'v0.2.2',
     dependencies = { 
       'nvim-lua/plenary.nvim',
       { 

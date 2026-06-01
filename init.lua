1077cdd44769afe5b533e3a49a487afc091619bb
Lin Wang <wanglinseven@gmail.com>
2026-06-02 01:21:56 +0800

    retab! init.lua

--- init.lua
+++ init.lua
@@ -15,9 +15,9 @@ vim.o.modified = false -- avoid warning when closing terminal buffer
 vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.o.shell = 'pwsh'
+	vim.o.shell = 'pwsh'
 else
-  vim.o.shell = 'fish'
+	vim.o.shell = 'fish'
 end
 
 vim.g.mapleader = ','
@@ -38,10 +38,10 @@ vim.keymap.set('x', '<leader>p', '"_dP', { noremap = true })
 vim.keymap.set('n', '<leader>v', '<C-v>', { noremap = true })
 
 vim.keymap.set('n', '<A-t>', function() 
-  local win_count = #(vim.api.nvim_list_wins())
-  if win_count == 1 then
-    vim.api.nvim_open_win(0, true, { split = 'left', win = 0 })
-  end
+	local win_count = #(vim.api.nvim_list_wins())
+	if win_count == 1 then
+		vim.api.nvim_open_win(0, true, { split = 'left', win = 0 })
+	end
 end, 
 { noremap = true }) 
 
@@ -50,12 +50,12 @@ local set_indent = function(n)
 	vim.o.shiftwidth = n
 	vim.o.softtabstop = -1 -- follows shiftwidth
 	vim.o.expandtab = false
-  vim.opt_local.smartindent = true
+	vim.opt_local.smartindent = true
 end
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = '*',
-  callback = function() set_indent(2) end
+	pattern = '*',
+	callback = function() set_indent(2) end
 })
 
 -----------------------------------------------------------------------------------------------------------------------
@@ -64,56 +64,56 @@ vim.api.nvim_create_autocmd('FileType', {
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  {
-    'AlexvZyl/nordic.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('nordic').load()
-    end
-  },
-  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = 'v0.2.2',
-    dependencies = { 
-      'nvim-lua/plenary.nvim',
-      { 
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -B build -D CMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-      },
-      'nvim-telescope/telescope-live-grep-args.nvim',
-    },
-    config = function()
-      require('telescope').load_extension('live_grep_args')
-    end
-  },
-  'neovim/nvim-lspconfig',
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = { 
-      'L3MON4D3/LuaSnip',
-      'hrsh7th/cmp-nvim-lsp'
-    }
-  },
-  {
-    'SmiteshP/nvim-navic',
-    dependencies = {
-      'neovim/nvim-lspconfig'
-    }
-  }
+	{
+		'AlexvZyl/nordic.nvim',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require('nordic').load()
+		end
+	},
+	{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = 'v0.2.2',
+		dependencies = { 
+			'nvim-lua/plenary.nvim',
+			{ 
+				'nvim-telescope/telescope-fzf-native.nvim',
+				build = 'cmake -S. -B build -D CMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+			},
+			'nvim-telescope/telescope-live-grep-args.nvim',
+		},
+		config = function()
+			require('telescope').load_extension('live_grep_args')
+		end
+	},
+	'neovim/nvim-lspconfig',
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = { 
+			'L3MON4D3/LuaSnip',
+			'hrsh7th/cmp-nvim-lsp'
+		}
+	},
+	{
+		'SmiteshP/nvim-navic',
+		dependencies = {
+			'neovim/nvim-lspconfig'
+		}
+	},
 })
 
 -----------------------------------------------------------------------------------------------------------------------
@@ -125,14 +125,14 @@ local telescope_builtin = require('telescope.builtin')
 local lga_actions = require('telescope-live-grep-args.actions')
 
 telescope.setup({
-  extensions = {
-    live_grep_args = {
-      auto_quoting = true,
-      mappings = {
-        ['<C-s>'] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-      }
-    }
-  }
+	extensions = {
+		live_grep_args = {
+			auto_quoting = true,
+			mappings = {
+				['<C-s>'] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+			}
+		}
+	}
 })
 
 vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {noremap = true})
@@ -141,30 +141,30 @@ vim.keymap.set('n', '<leader>fs', telescope.extensions.live_grep_args.live_grep_
 -- Bring up the previous search results.
 vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume)
 vim.keymap.set('v', '<leader>fs', function()
-  local text = '""'
-  local _, start_row, start_col, _ = unpack(vim.fn.getpos('v'))
-  local _, end_row, end_col, _ = unpack(vim.fn.getpos('.'))
-  if start_row == end_row then
-    local visual = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
-    if visual[1] then
-      text = '"' .. visual[1] .. '"'
-    end
-  end
-  local scope = ''
-  if vim.bo.filetype == 'cpp' then
-    scope = ' -tcpp'
-  elseif vim.bo.filetype == 'py' then
-    scope = ' -tpy'
-  elseif vim.bo.filetype == 'cmake'then
-    scope = ' -tcmake'
-  else
-    local buf_name = vim.fs.basename(vim.api.nvim_buf_get_name(0))
-    if buf_name == 'CMakeLists.txt' then
-        scope = ' -tcmake'
-    end
-  end
-  text = text .. scope
-  telescope.extensions.live_grep_args.live_grep_args({ default_text = text })
+	local text = '""'
+	local _, start_row, start_col, _ = unpack(vim.fn.getpos('v'))
+	local _, end_row, end_col, _ = unpack(vim.fn.getpos('.'))
+	if start_row == end_row then
+		local visual = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col, {})
+		if visual[1] then
+			text = '"' .. visual[1] .. '"'
+		end
+	end
+	local scope = ''
+	if vim.bo.filetype == 'cpp' then
+		scope = ' -tcpp'
+	elseif vim.bo.filetype == 'py' then
+		scope = ' -tpy'
+	elseif vim.bo.filetype == 'cmake'then
+		scope = ' -tcmake'
+	else
+		local buf_name = vim.fs.basename(vim.api.nvim_buf_get_name(0))
+		if buf_name == 'CMakeLists.txt' then
+				scope = ' -tcmake'
+		end
+	end
+	text = text .. scope
+	telescope.extensions.live_grep_args.live_grep_args({ default_text = text })
 end, {noremap = true})
 
 -----------------------------------------------------------------------------------------------------------------------
@@ -176,31 +176,31 @@ local luasnip = require('luasnip')
 
 local select_behavior = { behavior = cmp.SelectBehavior.Insert }
 cmp.setup({ 
-  snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
-  completion = { autocomplete = {cmp.TriggerEvent.TextChanged} },
-  sources = { 
-    { name = 'buffer', keyword_length = 2 },
-    { name = 'nvim_lsp', keyword_length = 2 },
-    { name = 'path', keyword_length = 2 },
-  },
-  window = { documentation = cmp.config.window.bordered() },
-  mapping = {
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item(select_behavior)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_behavior)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-  },
-  preselect = { cmp.PreselectMode.None },
+	snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
+	completion = { autocomplete = {cmp.TriggerEvent.TextChanged} },
+	sources = { 
+		{ name = 'buffer', keyword_length = 2 },
+		{ name = 'nvim_lsp', keyword_length = 2 },
+		{ name = 'path', keyword_length = 2 },
+	},
+	window = { documentation = cmp.config.window.bordered() },
+	mapping = {
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item(select_behavior)
+			else
+				fallback()
+			end
+		end, {'i', 's'}),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item(select_behavior)
+			else
+				fallback()
+			end
+		end, {'i', 's'}),
+	},
+	preselect = { cmp.PreselectMode.None },
 })
 
 -----------------------------------------------------------------------------------------------------------------------
@@ -214,68 +214,68 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -----------------------------------------------------------------------------------------------------------------------
 
 vim.lsp.config('clangd', {
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
 vim.lsp.enable('clangd')
 
 vim.lsp.config('pylsp', {
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
 vim.lsp.enable('pylsp')
 
 vim.lsp.config('rust_analyzer', {
-  capabilities = capabilities,
-  settings = {
-    ['rust-analyzer'] = {
-      rustfmt = { enableRangeFormatting = true },
-    },
-  },
+	capabilities = capabilities,
+	settings = {
+		['rust-analyzer'] = {
+			rustfmt = { enableRangeFormatting = true },
+		},
+	},
 })
 vim.lsp.enable('rust_analyzer')
 
 vim.lsp.config('glsl_analyzer', {
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
 vim.lsp.enable('glsl_analyzer')
 
 local navic = require('nvim-navic')
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-    if client.server_capabilities.documentSymbolProvider then
-      navic.attach(client, args.buf)
-    end
+		if client.server_capabilities.documentSymbolProvider then
+			navic.attach(client, args.buf)
+		end
 
-    if client.server_capabilities.definitionProvider then
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { buffer = args.buf })
-      vim.keymap.set('n', '<leader>gc', vim.lsp.buf.hover, { buffer = args.buf })
-    end
+		if client.server_capabilities.definitionProvider then
+			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { buffer = args.buf })
+			vim.keymap.set('n', '<leader>gc', vim.lsp.buf.hover, { buffer = args.buf })
+		end
 
-    if client.name == 'clangd' then
-      -- see: https://github.com/neovim/nvim-lspconfig/blob/master/lsp/clangd.lua#L96C50-L96C77
-      vim.keymap.set('n', '<leader>hs', '<cmd>LspClangdSwitchSourceHeader<cr>', { buffer = args.buf })
-    end
+		if client.name == 'clangd' then
+			-- see: https://github.com/neovim/nvim-lspconfig/blob/master/lsp/clangd.lua#L96C50-L96C77
+			vim.keymap.set('n', '<leader>hs', '<cmd>LspClangdSwitchSourceHeader<cr>', { buffer = args.buf })
+		end
 
-    if client.name == 'rust_analyzer' then
-      -- Format Rust code. N.B. there is no warning/error when formatting fails due to code errors.
-      vim.keymap.set('n', '<leader>rf',
-        function()
-          vim.lsp.buf.format()
-        end,
-        { noremap = true })
+		if client.name == 'rust_analyzer' then
+			-- Format Rust code. N.B. there is no warning/error when formatting fails due to code errors.
+			vim.keymap.set('n', '<leader>rf',
+				function()
+					vim.lsp.buf.format()
+				end,
+				{ noremap = true })
 
-      -- This automatically formats the rust on every write. But it's too slow.
-      -- vim.api.nvim_create_autocmd("BufWritePre", {
-      --   pattern = '*.rs',
-      --   buffer = bufnr,
-      --   callback = function()
-      --     vim.lsp.buf.format({ async = false })
-      --   end,
-      -- })
-    end
-  end,
+			-- This automatically formats the rust on every write. But it's too slow.
+			-- vim.api.nvim_create_autocmd("BufWritePre", {
+			--	 pattern = '*.rs',
+			--	 buffer = bufnr,
+			--	 callback = function()
+			--		 vim.lsp.buf.format({ async = false })
+			--	 end,
+			-- })
+		end
+	end,
 })
 
 ----------------------------------------------------------------------------------------------------
@@ -283,8 +283,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
 ----------------------------------------------------------------------------------------------------
 
 vim.diagnostic.config({
-  underline = false, -- Windows terminal don't support swiggly underline.
-  signs = false,
-  virtual_text = true,
+	underline = false, -- Windows terminal don't support swiggly underline.
+	signs = false,
+	virtual_text = true,
 })
 
